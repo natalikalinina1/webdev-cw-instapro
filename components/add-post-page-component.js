685a53eca8b27214b0a26a6e39b189bfdbf1181e
natalikalinina1 <kidmil@mail.ru>
2023-05-16 13:08:08 +0300

    addpost

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -1,11 +1,18 @@
+import { renderHeaderComponent } from "./header-component.js";
+
 export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   const render = () => {
-    // TODO: Реализовать страницу добавления поста
+    // Страница добавления поста
     const appHtml = `
     <div class="page-container">
       <div class="header-container"></div>
-      Cтраница добавления поста
-      <button class="button" id="add-button">Добавить</button>
+  <div class="form">
+        <h3 class="form-title">Добавить пост</h3></div>
+        <div class="form-inputs">        
+          <button class="button" id="add-button">Выберите фото</button>
+          <textarea class="textarea add-post-textarea" rows="4" placeholder="Опишите фотографию:"></textarea>
+        </div>
+      </div>
     </div>
   `;
 
@@ -17,7 +24,13 @@ export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
         imageUrl: "https://image.png",
       });
     });
-  };
+  
+
+  renderHeaderComponent({
+    element: document.querySelector(".header-container"),
+  });
+};
+
 
   render();
 }
